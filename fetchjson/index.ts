3c34807b558bb56f8fd2a44b8e90273f5955b92e
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-08-27 00:00:40 -0500

    more types

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -13,11 +13,15 @@ axios.get(url).then((response) => {
 
   const ID = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
-  console.log(`
-    The Todo with ID: ${ID}
-    Has a title of : ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(ID, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of : ${title}
+    Is it finished? ${completed}
+  `);
+};
