f5554b4b721f7390e52609f0c153fd1fe6d83308
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-08-31 22:33:27 -0500

    refactored interface

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,19 +1,18 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVeicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVeicle(oldCivic);
+printSummary(oldCivic);
