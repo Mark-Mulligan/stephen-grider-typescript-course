7ba7ab28a0d44ed63aa28c74a643a46c3eb4e390
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-09-02 08:49:29 -0500

    classes in typescript

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -14,9 +14,9 @@ let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {}
+class Test {}
 
-let car: Car = new Car();
+let test: Test = new Test();
 
 // Object literal
 let point: { x: number; y: number } = {
