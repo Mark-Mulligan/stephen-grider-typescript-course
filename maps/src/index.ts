669a1876fa074d3f2491f7448db63af4e8d9594e
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-09-03 14:17:49 -0500

    added google maps to project

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './User';
 import { Company } from './Company';
 
@@ -8,3 +10,11 @@ console.log(user);
 const company = new Company();
 
 console.log(company);
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
