f2d6aef93e644693c780e26c263bbc02d8655362
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-09-24 23:57:01 -0500

    worked on web classes

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Mark Mulligan', age: 27 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('Change #1');
+user.events.on('change', () => {
+  console.log('change!');
 });
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered');
-});
+
+user.events.trigger('change');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,38 +1,22 @@
-interface userProps {
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
 
-  constructor(private data: userProps) {}
+  constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
   }
 
-  set(update: userProps): void {
+  set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 }
