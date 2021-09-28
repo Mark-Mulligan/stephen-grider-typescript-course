caecfab6540f1e1960301935c133444feaa99c9e
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-09-28 00:49:09 -0500

    finished server project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,15 @@
 import express from 'express';
-import { router } from './routes/loginRoutes';
 import cookieSession from 'cookie-session';
 
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieSession({ keys: ['laskdjf'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
